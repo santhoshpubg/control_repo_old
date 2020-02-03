042edbd9f1bd88ec84b95f3723078133fc5d2c39
santhoshpubg <56072733+santhoshpubg@users.noreply.github.com>
2020-02-03 14:15:44 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,5 @@
-node default {
-#file {'/root/README':
- #       ensure  => file,
-  #      content => 'this is a readme',
-   #     owner   => 'root'
-    #  }
- #file {'/root/README':
-      #  ensure  => file,
-      #  content => 'this is a readme',
-       # owner   => 'root     
+node default {     
+}
+node 'master.puppet.vm'{
+include role::master_server
 }
